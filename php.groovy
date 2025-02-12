9d2885cdc756c82bdd76d44947fa29a7fa08208c
yagza@mail.ru <yagza@mail.ru>
2025-02-12 16:02:09 +0300

    variables

--- php.groovy
+++ php.groovy
@@ -1,12 +1,12 @@
 timestamps {
     ansiColor('xterm') {
-        node(env.JenkinsSlaveNode) {
+        node(JenkinsSlaveNode) {
 
             env.PROJECT_NAME = "project_pso"
             env.PROJECT_VERSION = "0.0.1"
 
             stage ('Git Checkout') {
-                checkout scmGit(branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[credentialsId: 'jenkins-to-github', url: 'git@github.com:yagza/simple-php-website.git']])
+                checkout scmGit(branches: [[name: GitBranchSource]], extensions: [], userRemoteConfigs: [[credentialsId: GithubCreds, url: GitUrlSource]])
             }
         }
     }
