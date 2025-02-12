0ff26cbbcf2476aacec2e7869e44d6ac2b2aaca3
yagza@mail.ru <yagza@mail.ru>
2025-02-12 15:47:34 +0300

    try normal checkout

--- php.groovy
+++ php.groovy
@@ -6,9 +6,7 @@ timestamps {
             env.PROJECT_VERSION = "0.0.1"
 
             stage ('Git Checkout') {
-                println("prrrrrrr")
-                //git credentialsId: 'jenkins-to-github', url: 'git@github.com:yagza/simple-php-website.git'
-                // checkout scmGit(branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[credentialsId: 'jenkins-to-github', url: 'git@github.com:yagza/simple-php-website.git']])
+                checkout scmGit(branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[credentialsId: 'jenkins-to-github', url: 'git@github.com:yagza/simple-php-website.git']])
             }
         }
     }
