9dc76e78b4afd4ce16929ac3b2f0fdaa941238cc
yagza@mail.ru <yagza@mail.ru>
2025-02-12 15:12:02 +0300

    psooo

--- php.groovy
+++ php.groovy
@@ -5,8 +5,9 @@ timestamps {
             env.PROJECT_NAME = "project_pso"
             env.PROJECT_VERSION = "0.0.1"
 
-            stage ("Git Checkout") {
-                checkout scmGit(branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[credentialsId: 'jenkins-to-github', url: 'git@github.com:yagza/simple-php-website.git']])
+            stage ('Git Checkout') {
+                git credentialsId: 'jenkins-to-github', url: 'git@github.com:yagza/simple-php-website.git'
+                // checkout scmGit(branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[credentialsId: 'jenkins-to-github', url: 'git@github.com:yagza/simple-php-website.git']])
             }
         }
     }
