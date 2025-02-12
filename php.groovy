4319c64d2ebdaf4e98e2125346284169b8f72d24
yagza@mail.ru <yagza@mail.ru>
2025-02-12 15:26:55 +0300

    not everybody

--- php.groovy
+++ php.groovy
@@ -1,12 +1,13 @@
 timestamps {
     ansiColor('xterm') {
-        node(env.JenkinsSlaveNode) {
+        // node(env.JenkinsSlaveNode) {
+        node {
 
             env.PROJECT_NAME = "project_pso"
             env.PROJECT_VERSION = "0.0.1"
 
             stage ('Git Checkout') {
-                println("Вы все пидоры")
+                println("ладно. не все")
                 //git credentialsId: 'jenkins-to-github', url: 'git@github.com:yagza/simple-php-website.git'
                 // checkout scmGit(branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[credentialsId: 'jenkins-to-github', url: 'git@github.com:yagza/simple-php-website.git']])
             }
