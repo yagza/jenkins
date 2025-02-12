725f9081f1200be73d0c0ba13689465ed992773c
yagza@mail.ru <yagza@mail.ru>
2025-02-12 15:31:58 +0300

    trrr

--- php.groovy
+++ php.groovy
@@ -6,7 +6,7 @@ timestamps {
             env.PROJECT_VERSION = "0.0.1"
 
             stage ('Git Checkout') {
-                println("ладно. не все")
+                println("совсем не все")
                 //git credentialsId: 'jenkins-to-github', url: 'git@github.com:yagza/simple-php-website.git'
                 // checkout scmGit(branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[credentialsId: 'jenkins-to-github', url: 'git@github.com:yagza/simple-php-website.git']])
             }
