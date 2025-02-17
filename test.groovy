3dda4c97b7c2c123591f58ae762a06cdcc5ac4b6
yagza@mail.ru <yagza@mail.ru>
2025-02-17 15:42:54 +0300

    stages

--- test.groovy
+++ test.groovy
@@ -5,12 +5,17 @@ timestamps {
         ENV_INSIDE_NODE_2 = env.ENV_FROM_PIPE_2
     }
         try {
-            println("These are external defined envs")
-            println(ENV_FROM_PIPE_1)
-            println(env.ENV_FROM_PIPE_2)
-            println("These are inside defined envs")
-            echo "${ENV_INSIDE_NODE_2}"
-            echo "${ENV_INSIDE_NODE_1}"
+
+            stage ("Print external defined envs") {
+                println(ENV_FROM_PIPE_1)
+                println(env.ENV_FROM_PIPE_2)
+            }
+
+            stage ("Print inside defined envs") {
+                echo "${ENV_INSIDE_NODE_2}"
+                echo "${ENV_INSIDE_NODE_1}"
+            }
+            
         } catch (Exception e) {
             echo "Pipeline failed: ${e.getMessage()}"
             currentBuild.result = 'FAILURE'
