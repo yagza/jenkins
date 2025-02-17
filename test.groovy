d04adf3a8471d54cb422f75a7297e75c141d278d
yagza@mail.ru <yagza@mail.ru>
2025-02-17 15:46:36 +0300

    oo

--- test.groovy
+++ test.groovy
@@ -1,10 +1,17 @@
 timestamps {
     node(JenkinsSlaveNode) {
+/*
     environment {
         ENV_INSIDE_NODE_1 = 'This is the value of ENV_INSIDE_NODE_1'
         ENV_INSIDE_NODE_2 = env.ENV_FROM_PIPE_2
     }
+*/
         try {
+
+            script {
+                env.ENV_INSIDE_NODE_1 = 'This is the value of ENV_INSIDE_NODE_1'
+                env.ENV_INSIDE_NODE_2 = env.ENV_FROM_PIPE_2
+            }
 
             stage ("Print external defined envs") {
                 println(ENV_FROM_PIPE_1)
@@ -15,7 +22,7 @@ timestamps {
                 echo "${ENV_INSIDE_NODE_2}"
                 echo "${ENV_INSIDE_NODE_1}"
             }
-            
+
         } catch (Exception e) {
             echo "Pipeline failed: ${e.getMessage()}"
             currentBuild.result = 'FAILURE'
