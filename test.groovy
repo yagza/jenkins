2b986aa2a2c921a8cc4fa4605a5357f2d4036fe6
yagza@mail.ru <yagza@mail.ru>
2025-02-17 16:01:58 +0300

    body

--- test.groovy
+++ test.groovy
@@ -9,32 +9,33 @@ timestamps {
     node(JenkinsSlaveNode) {
 
         withEnv(['ENV_INSIDE_NODE_1="This is the value of ENV_INSIDE_NODE_1"',
-             'ENV_INSIDE_NODE_2=env.ENV_FROM_PIPE_2'])
+             'ENV_INSIDE_NODE_2=env.ENV_FROM_PIPE_2']) {
 
-        try {
-/*
-            script {
-                env.ENV_INSIDE_NODE_1 = 'This is the value of ENV_INSIDE_NODE_1'
-                env.ENV_INSIDE_NODE_2 = env.ENV_FROM_PIPE_2
+            try {
+/*  
+                script {
+                    env.ENV_INSIDE_NODE_1 = 'This is the value of ENV_INSIDE_NODE_1'
+                    env.ENV_INSIDE_NODE_2 = env.ENV_FROM_PIPE_2
+                }
+*/  
+                stage ("Print external defined envs") {
+                    println(ENV_FROM_PIPE_1)
+                    println(env.ENV_FROM_PIPE_2)
+                }
+    
+                stage ("Print inside defined envs") {
+                    echo "${ENV_INSIDE_NODE_2}"
+                    echo "${ENV_INSIDE_NODE_1}"
+                }
+    
+            } catch (Exception e) {
+                echo "Pipeline failed: ${e.getMessage()}"
+                currentBuild.result = 'FAILURE'
+                throw e 
+            } finally {
+                println("Очистка Jenkins Slave Node")
+                cleanWs()
             }
-*/
-            stage ("Print external defined envs") {
-                println(ENV_FROM_PIPE_1)
-                println(env.ENV_FROM_PIPE_2)
-            }
-
-            stage ("Print inside defined envs") {
-                echo "${ENV_INSIDE_NODE_2}"
-                echo "${ENV_INSIDE_NODE_1}"
-            }
-
-        } catch (Exception e) {
-            echo "Pipeline failed: ${e.getMessage()}"
-            currentBuild.result = 'FAILURE'
-            throw e 
-        } finally {
-            println("Очистка Jenkins Slave Node")
-            cleanWs()
         }
     }
 }
