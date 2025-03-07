a3ff5bf045861f580ea2711442326179a5ad7cf3
yagza@mail.ru <yagza@mail.ru>
2025-03-07 09:28:41 +0300

    maven

--- maven.groovy
+++ maven.groovy
@@ -1,19 +1,26 @@
 timestamps {
     node(JenkinsSlaveNode) {
 
+        MAVEN_VERSION = "${MAVEN_VERSION}"
+        JDK_VERSION = "${JDK_VERSION}"
+
             try {
 
                 stage ('Git Checkout') {
                     checkout scmGit(branches: [[name: GitBranchSource]], extensions: [], userRemoteConfigs: [[credentialsId: GithubCreds, url: GitUrlSource]])
 
                 stage('Build') {
-                    steps {
-                        sh 'mvn -B -DskipTests clean package'
+                    withMaven(maven: "${MAVEN_VERSION}", jdk: "${JDK_VERSION}") {
+                        steps {
+                            //sh 'mvn -B -DskipTests clean package'
+                            sh 'mvn -B -DskipTests clean package'
+                        }
                     }
                 }
                 stage('Test') {
                     steps {
-                        sh 'mvn test'
+                        //sh 'mvn test'
+                        sh 'mvn -B -DskipTests clean package'
                     }
                     post {
                         always {
