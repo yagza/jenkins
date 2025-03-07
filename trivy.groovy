8b55b495c8154fea4167ac88752a0c58b2ff5aee
yagza@mail.ru <yagza@mail.ru>
2025-03-07 14:01:13 +0300

    trivy

---
+++ trivy.groovy
@@ -0,0 +1,30 @@
+timestamps {
+    node(JenkinsSlaveNode) {
+
+        def imageName = "yagza/shadowsocks_v2ray"
+        def trivyReport = ${env.imageName}.replaceAll("/","_")
+
+        try {
+            stage ("trivy scan") {
+                sh "trivy image --format template --template \"@/usr/local/bin/contrib/html.tpl\" -o ${trivyReport} ${imageName}"
+            }
+
+            stage('Archive Report') {
+                archiveArtifacts artifacts: trivyReport, fingerprint: true
+            }
+
+            stage('Notify') {
+                echo "Trivy scan completed. Report is available as an artifact."
+            }
+
+        } catch (Exception e) {
+            echo "Pipeline failed: ${e.getMessage()}"
+            currentBuild.result = 'FAILURE'
+            throw e 
+
+        } finally {
+            println("Очистка Jenkins Slave Node")
+            cleanWs()
+        }
+    }
+}
