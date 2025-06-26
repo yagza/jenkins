5991a57fb1c97dfb038caf0f739347a0dd4e7da3
yagza <yagza@mail.ru>
2025-06-26 12:37:32 +0300

    Update vault-test.groovy

--- vault-test.groovy
+++ vault-test.groovy
@@ -2,7 +2,9 @@ timestamps {
   node(JenkinsSlaveNode) {
 
     try {
-
+   
+      
+      stage ("vault secret print") {
         def secrets = [
           [path: 'team/btccpl', engineVersion: 2, secretValues: [
             [envVar: 'SECRET_1', vaultKey: 'some_name'],
@@ -12,10 +14,8 @@ timestamps {
           ]
         ]
 
-        def vault_configuration = [vaultUrl: 'http://10.0.0.150:8200', vaultCredentialId: 'btccpl-read-vault', engineVersion: 2]      
-      
-      stage ("vault secret print") {
-    
+        def vault_configuration = [vaultUrl: 'http://10.0.0.150:8200', vaultCredentialId: 'btccpl-read-vault', engineVersion: 2]
+          
         withVault([configuration: vault_configuration, vaultSecrets: secrets]) {
           sh 'echo $SECRET_1'
           sh 'echo $SECRET_2'
